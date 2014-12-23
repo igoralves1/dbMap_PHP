ba0a0ef4d0421929c81075073c8bec372a80eb3c
Igor Alves <igoralves1@hotmail.com>
2014-12-23 13:01:22 -0500

    Class directory

---
+++ myCLASS/dbMap_PHP.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of dbMap_PHP
+ *Esta classe irá criar uma outra classe que será o mapa da base de dados selecionada pelo usuário.
+ *A classe criada será de natureza estática, o que permite acesso aos métodos sem a necessidade de se criar objetos.
+ * 
+ * NOTA:
+ * Esta é a primeira versão do projeto, versão teste. 
+ * Logo, ela não será preparada para gerar conexões PDO_MySQL  ou MySQLi.
+ *  
+ * @author Igor
+ */
+class myDbMap {
+ //put your code here
+}
